0d3f5f6b0e7e06eea869922e744931004722a421
arpitjhajharia <arpitjhajharia@gmail.com>
2026-01-28 15:31:24 +0530

    Deploy command added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/BWGAnti1/", // <--- ADD THIS LINE (Must match your Repo name exactly)
 })
